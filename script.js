ceba6d1cf540f5a2090913af92ccd9bf1e2b1d8f
OlekKaniuk <oleksandrkaniuka994@gmail.com>
2026-01-28 15:42:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function generate() {
     let str = '';
     for (let i = 1; i <= 8; i ++) {
-        str += `<div class = "posters" id = 'foto-0${i}'><p><img src = 'images/foto-0${i}.jpg' alt = 'foto-0${i}' width = '230' height = '270'></p>
+        str += `<div class = "posters" id = 'foto-0${i}'><p><img src = 'foto-0${i}.jpg' alt = 'foto-0${i}' width = '230' height = '270'></p>
 
 
 </div>`
     }
     document.getElementById('platf').innerHTML = str;
+
 }
