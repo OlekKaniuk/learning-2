4e52dcfedab8165bdf103ceda71012a954bd3548
OlekKaniuk <oleksandrkaniuka994@gmail.com>
2026-01-28 15:33:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function generate() {
+    let str = '';
+    for (let i = 1; i <= 8; i ++) {
+        str += `<div class = "posters" id = 'foto-0${i}'><p><img src = 'images/foto-0${i}.jpg' alt = 'foto-0${i}' width = '230' height = '270'></p>
+
+
+</div>`
+    }
+    document.getElementById('platf').innerHTML = str;
+}
